c0bc2ba5ea847c1a53836881125fc2d03b730817
Bram <35614609+BrammyS@users.noreply.github.com>
2024-03-06 09:34:47 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ mod utils;
 fn log_request(req: &Request) {
     console_log!(
         "{} - [{}], located at: {:?}, within: {}",
-        Date::now().to_string(),
+        req.method().to_string(),
         req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
+        req.cf().unwrap().coordinates().unwrap_or_default(),
+        req.cf().unwrap().region().unwrap_or("unknown region".into())
     );
 }
 
